e15ed488161c7671b40dda72c7980ad054b6c5ec
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-06 08:38:58 +0100

    Update AppDelegateDeeplink.m

--- src/ios/AppDelegateDeeplink.m
+++ src/ios/AppDelegateDeeplink.m
@@ -8,23 +8,23 @@ static NSString *const PLUGIN_NAME = @"UniversalLinks";
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
 
     if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
-        NSLog(@"[UniversalLinks] Ignorado: Tipo de atividade ou URL inválida");
+        NSLog(@"[UniversalLinks] invalid url");
         return NO;
     }
     
     CULPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
     if (plugin == nil) {
-        NSLog(@"[UniversalLinks] Plugin não encontrado");
+        NSLog(@"[UniversalLinks] Plugin not found");
         return NO;
     }
 
     // Show deeplink
-    NSLog(@"[UniversalLinks] Recebido URL: %@", userActivity.webpageURL.absoluteString);
+    NSLog(@"[UniversalLinks] URL: %@", userActivity.webpageURL.absoluteString);
     
     BOOL handled = [plugin handleUserActivity:userActivity];
 
     // show if it is a valid universal link
-    NSLog(@"[UniversalLinks] Resultado do handleUserActivity: %@", handled ? @"YES" : @"NO");
+    NSLog(@"[UniversalLinks] handleUserActivity result: %@", handled ? @"YES" : @"NO");
 
     return handled;
 }
