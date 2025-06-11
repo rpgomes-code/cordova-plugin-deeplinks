aa06d908bcfcdc250b73afcef24923db9faf9416
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-11 11:26:45 +0100

    Create NOSLinksPlugin.m

---
+++ src/ios/NOSLinksPlugin.m
@@ -0,0 +1,21 @@
+#import "NOSLinksPlugin.h"
+
+@implementation NOSLinksPlugin
+
+- (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
+    if (userActivity.webpageURL == nil) {
+        NSLog(@"[NOSLinks] No URL to handle.");
+        return NO;
+    }
+
+    NSString *urlString = userActivity.webpageURL.absoluteString;
+    NSLog(@"[NOSLinks] Handling universal link: %@", urlString);
+
+    // Enviar para JS se quiseres
+    NSString *js = [NSString stringWithFormat:@"window.nosLinks && window.nosLinks.onDeepLink && window.nosLinks.onDeepLink('%@');", urlString];
+    [self.commandDelegate evalJs:js];
+
+    return YES;
+}
+
+@end
