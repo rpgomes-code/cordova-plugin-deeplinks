ad3b4789a1469dc5ca0681e5ee62ca3d51f87371
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-11 14:12:38 +0100

    Update NOSLinks.js

--- www/NOSLinks.js
+++ www/NOSLinks.js
@@ -1,7 +1,5 @@
-module.exports = {
+window.nosLinks = {
     onDeepLink: function (url) {
-        console.log("[NOSLinks] Received URL: ", url);
-        // Aqui podes dispatchar eventos, etc.
         const event = new CustomEvent("noslink", { detail: { url } });
         window.dispatchEvent(event);
     }
