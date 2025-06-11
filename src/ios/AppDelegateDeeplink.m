fe2a0eb5e91b67ed5e32e49b5fb33efcaf84c7f3
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-11 11:24:08 +0100

    Update AppDelegateDeeplink.m

--- src/ios/AppDelegateDeeplink.m
+++ src/ios/AppDelegateDeeplink.m
@@ -1,19 +1,41 @@
+#import "AppDelegate+NOSLinksPlugin.h"
 #import "NOSLinksPlugin.h"
 
-@implementation NOSLinksPlugin
+static NSString *const PLUGIN_NAME = @"NOSLinks";
 
-- (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
-    if (userActivity.webpageURL == nil) {
-        NSLog(@"[NOSLinks] No URL to handle.");
+@implementation AppDelegate (NOSLinksPlugin)
+
+// Universal Link handler
+- (BOOL)application:(UIApplication *)application 
+continueUserActivity:(NSUserActivity *)userActivity 
+restorationHandler:(void (^)(NSArray *))restorationHandler {
+
+    if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
+        NSLog(@"[NOSLinks] Invalid URL");
         return NO;
     }
 
-    NSString *urlString = userActivity.webpageURL.absoluteString;
-    NSLog(@"[NOSLinks] Handling universal link: %@", urlString);
+    NOSLinksPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
+    if (plugin == nil) {
+        NSLog(@"[NOSLinks] Plugin not found");
+        return NO;
+    }
 
-    // Enviar para JS se quiseres
-    NSString *js = [NSString stringWithFormat:@"window.nosLinks && window.nosLinks.onDeepLink && window.nosLinks.onDeepLink('%@');", urlString];
-    [self.commandDelegate evalJs:js];
+    NSLog(@"[NOSLinks] URL: %@", userActivity.webpageURL.absoluteString);
+
+    BOOL handled = [plugin handleUserActivity:userActivity];
+
+    NSLog(@"[NOSLinks] handleUserActivity result: %@", handled ? @"YES" : @"NO");
+
+    return handled;
+}
+
+// Deep link (URL scheme) handler
+- (BOOL)application:(UIApplication *)app 
+            openURL:(NSURL *)url 
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+
+    NSLog(@"[NOSLinks] App opened via URL scheme: %@", url.absoluteString);
 
     return YES;
 }
