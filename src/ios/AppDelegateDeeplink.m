2536fdeddaccfd717ad87b0a38690ecdf80c8986
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-11 11:23:33 +0100

    Update AppDelegateDeeplink.m

--- src/ios/AppDelegateDeeplink.m
+++ src/ios/AppDelegateDeeplink.m
@@ -1,42 +1,20 @@
-#import "AppDelegate+CULPlugin.h"
-#import "CULPlugin.h"
+#import "NOSLinksPlugin.h"
 
-static NSString *const PLUGIN_NAME = @"NOSLinks";
+@implementation NOSLinksPlugin
 
-@implementation AppDelegate (Deeplink)
-
-// Universal Link handler
-- (BOOL)application:(UIApplication *)application 
-continueUserActivity:(NSUserActivity *)userActivity 
-restorationHandler:(void (^)(NSArray *))restorationHandler {
-
-    if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
-        NSLog(@"[UniversalLinks] invalid url");
-        return NO;
-    }
-    
-    CULPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
-    if (plugin == nil) {
-        NSLog(@"[UniversalLinks] Plugin not found");
+- (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
+    if (userActivity.webpageURL == nil) {
+        NSLog(@"[NOSLinks] No URL to handle.");
         return NO;
     }
 
-    NSLog(@"[UniversalLinks] URL: %@", userActivity.webpageURL.absoluteString);
-    
-    BOOL handled = [plugin handleUserActivity:userActivity];
+    NSString *urlString = userActivity.webpageURL.absoluteString;
+    NSLog(@"[NOSLinks] Handling universal link: %@", urlString);
 
-    NSLog(@"[UniversalLinks] handleUserActivity result: %@", handled ? @"YES" : @"NO");
+    // Enviar para JS se quiseres
+    NSString *js = [NSString stringWithFormat:@"window.nosLinks && window.nosLinks.onDeepLink && window.nosLinks.onDeepLink('%@');", urlString];
+    [self.commandDelegate evalJs:js];
 
-    return handled;
-}
-
-// Deep link (URL scheme) handler
-- (BOOL)application:(UIApplication *)app 
-            openURL:(NSURL *)url 
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-
-    NSLog(@"[DeepLink] App opened via URL scheme: %@", url.absoluteString);
-    
     return YES;
 }
 
