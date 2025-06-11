b11d5aea81cad298e7509ec812e4a220ed682eff
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-11 11:27:12 +0100

    Delete src/ios/AppDelegate+NOSLinksPlugin.h

--- src/ios/AppDelegate+NOSLinksPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface NOSLinksPlugin : CDVPlugin
-
-- (BOOL)handleUserActivity:(NSUserActivity *)userActivity;
-
-@end
