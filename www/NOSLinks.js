b98c0d41bfd01135d1123814d5d16e55cb2b1b2d
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-11 17:12:10 +0100

    Update NOSLinks.js

--- www/NOSLinks.js
+++ www/NOSLinks.js
@@ -2,7 +2,7 @@ var PLUGIN_NAME = "NOSLinks";
 
 window.NOSLinks = {
     onDeepLink: function (url) {
-        const event = new CustomEvent("NOSLinks", { detail: { url } });
+        const event = new CustomEvent("noslink", { detail: { url } });
         window.dispatchEvent(event);
     }
 };
