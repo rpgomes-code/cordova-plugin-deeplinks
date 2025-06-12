aeb3a943da1ec4ac05b3d84108f3ea019dbd18d6
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-12 09:56:25 +0100

    Update NOSLinksPlugin.m

--- src/ios/NOSLinksPlugin.m
+++ src/ios/NOSLinksPlugin.m
@@ -4,6 +4,15 @@
 
 static NSString *pendingURL = nil;
 
+- (void)pluginInitialize {
+    if (pendingURL != nil) {
+        NSString *js = [NSString stringWithFormat:@"window.NOSLinks && window.NOSLinks.onDeepLink && window.NOSLinks.onDeepLink('%@');", pendingURL];
+        [self.commandDelegate evalJs:js];
+        NSLog(@"[NOSLinks] Fire pending universal link: %@", pendingURL);
+        pendingURL = nil;
+    }
+}
+
 - (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
     if (userActivity.webpageURL == nil) return NO;
 
