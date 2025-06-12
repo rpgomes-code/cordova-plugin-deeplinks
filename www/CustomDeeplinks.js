1d07bf74f5b2a1ae4a4a5cc682d0740e04ec1fd5
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-12 12:13:43 +0100

    Update CustomDeeplinks.js

--- www/CustomDeeplinks.js
+++ www/CustomDeeplinks.js
@@ -6,3 +6,25 @@ window.CustomDeeplinksPlugin = {
         window.dispatchEvent(event);
     }
 };
+
+CustomDeeplinks.getPendingDeeplink = function (callback) {
+    if (!window.cordova || !cordova.exec) {
+        console.warn("Cordova not ready");
+        callback(null);
+        return;
+    }
+    cordova.exec(
+        function (url) {
+            if (typeof callback === 'function') {
+                callback(url);
+            }
+        },
+        function (err) {
+            console.error("Cannot get pending deeplink:", err);
+            callback(null);
+        },
+        "CustomDeeplinks",
+        "getPendingDeeplink",
+        []
+    );
+};
