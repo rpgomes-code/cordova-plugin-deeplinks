a32fc533878721fcee80d21d490396ee92f75de8
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-06-18 10:49:15 +0100

    Update CustomDeeplinksPlugin.java

--- src/android/CustomDeeplinksPlugin.java
+++ src/android/CustomDeeplinksPlugin.java
@@ -5,7 +5,10 @@ import android.net.Uri;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
