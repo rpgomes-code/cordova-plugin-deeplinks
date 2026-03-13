d01c79d8d370587481576ff797846241b43d701b
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-03-13 15:45:21 +0000

    Update CustomDeeplinksPlugin.m

--- src/ios/CustomDeeplinksPlugin.m
+++ src/ios/CustomDeeplinksPlugin.m
@@ -27,7 +27,6 @@ static NSString *pendingURL = nil;
         NSString *js = [NSString stringWithFormat:@"window.CustomDeeplinks && window.CustomDeeplinks.onDeepLink && window.CustomDeeplinks.onDeepLink('%@');", escapedURL];
         [self.commandDelegate evalJs:js];
         NSLog(@"[CustomDeeplinks] Fire universal link immediately: %@", urlString);
-        pendingURL = nil;
     }
 
     return YES;
