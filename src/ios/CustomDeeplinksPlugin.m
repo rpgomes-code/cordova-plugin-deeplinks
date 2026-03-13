5570f7e299de28456eb12a318a047a394e2ff4fd
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-03-13 15:59:05 +0000

    Update CustomDeeplinksPlugin.m

--- src/ios/CustomDeeplinksPlugin.m
+++ src/ios/CustomDeeplinksPlugin.m
@@ -4,13 +4,19 @@
 
 static NSString *pendingURL = nil;
 
+// Static method to allow the AppDelegate to write the URL without needing the active instance.
++ (void)setPendingURL:(NSString *)url {
+    pendingURL = url;
+}
+
 - (void)pluginInitialize {
+    // The immediate clearing of the pendingURL has been removed here to allow JS to consume it via getPendingDeeplink if preferred.
     if (pendingURL != nil) {
         NSString *escapedURL = [pendingURL stringByReplacingOccurrencesOfString:@"'" withString:@"\\'"];
         NSString *js = [NSString stringWithFormat:@"window.CustomDeeplinks && window.CustomDeeplinks.onDeepLink && window.CustomDeeplinks.onDeepLink('%@');", escapedURL];
         [self.commandDelegate evalJs:js];
         NSLog(@"[CustomDeeplinks] Fire pending universal link: %@", pendingURL);
-        pendingURL = nil;
+        // pendingURL = nil; // Comentado para evitar race conditions com o getPendingDeeplink do JS
     }
 }
 
@@ -44,7 +50,8 @@ static NSString *pendingURL = nil;
     } else {
         NSLog(@"[CustomDeeplinks] No pending URL");
         
-        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
+        // Return OK with nil instead of NO_RESULT to make handling easier in JS.
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:nil];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     }
 }
