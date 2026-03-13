1f64722e08ac940194f1b814481a1d5d77acdc3a
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-03-13 17:28:54 +0000

    Update CustomDeeplinksPlugin.m

--- src/ios/CustomDeeplinksPlugin.m
+++ src/ios/CustomDeeplinksPlugin.m
@@ -11,16 +11,34 @@ static NSString *pendingURL = nil;
 
 - (void)pluginInitialize {
     if (pendingURL != nil) {
-        NSLog(@"[CustomDeeplinks] Plugin initialized. URL waiting: %@", pendingURL);
+        NSLog(@"[CustomDeeplinks] Plugin initialized. URL waiting for JS: %@", pendingURL);
     }
 }
 
+// Este método é chamado pelo AppDelegate quando a app já está aberta
 - (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
     if (userActivity.webpageURL == nil) return NO;
 
     NSString *urlString = userActivity.webpageURL.absoluteString;
+    NSLog(@"[CustomDeeplinks] Handling active link: %@", urlString);
+
+    // 1. Guardamos na mesma para o caso de o JS ainda não ter feito o check
     pendingURL = urlString;
-    
+
+    // 2. Disparamos o evento CustomEvent para o seu window.addEventListener("deeplinks")
+    if (self.webViewEngine && self.webViewEngine.engineWebView) {
+        // MODIFICAÇÃO: Dispara o evento "deeplinks" que o seu JS espera
+        NSString *js = [NSString stringWithFormat:
+            @"var evt = new CustomEvent('deeplinks', { detail: { url: '%@' } }); window.dispatchEvent(evt);", 
+            urlString];
+            
+        [self.commandDelegate evalJs:js];
+        NSLog(@"[CustomDeeplinks] Event 'deeplinks' dispatched to JS");
+        
+        // Limpamos o pendingURL porque o evento já o entregou em "tempo real"
+        pendingURL = nil;
+    }
+
     return YES;
 }
 
@@ -30,7 +48,7 @@ static NSString *pendingURL = nil;
     CDVPluginResult *result;
     if (pendingURL != nil && ![pendingURL isEqualToString:@""]) {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:pendingURL];
-        pendingURL = nil; // SÓ LIMPA AQUI após entregar ao JS
+        pendingURL = nil; 
     } else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:nil];
     }
