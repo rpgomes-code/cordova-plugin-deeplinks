4bbd042c537387077a42a04afdb8e5d7bf6d6cd2
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-03-16 12:16:05 +0000

    Update CustomDeeplinksPlugin.m

--- src/ios/CustomDeeplinksPlugin.m
+++ src/ios/CustomDeeplinksPlugin.m
@@ -44,28 +44,4 @@ static NSString *pendingURL = nil;
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
-@end    NSString *urlString = userActivity.webpageURL.absoluteString;
-    NSLog(@"[CustomDeeplinks] Handling Universal Link: %@", urlString);
-    
-    [self handleUrl:urlString];
-    return YES;
-}
-
-// Cordova Command: Called by JS on startup to catch Cold Starts
-- (void)getPendingDeeplink:(CDVInvokedUrlCommand *)command {
-    NSLog(@"[CustomDeeplinks] JS polling for pending URL. Current: %@", pendingURL);
-
-    CDVPluginResult *result;
-    if (pendingURL != nil && ![pendingURL isEqualToString:@""]) {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:pendingURL];
-        
-        // Clear after consumption to avoid processing the same link twice
-        pendingURL = nil; 
-    } else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:nil];
-    }
-    
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-}
-
 @end
