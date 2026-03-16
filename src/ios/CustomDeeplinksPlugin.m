26c1452d934c65f3589ae88fe2a168996fe78ed4
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-03-16 11:33:13 +0000

    Update CustomDeeplinksPlugin.m

--- src/ios/CustomDeeplinksPlugin.m
+++ src/ios/CustomDeeplinksPlugin.m
@@ -6,35 +6,45 @@ static NSString *pendingURL = nil;
 
 + (void)setPendingURL:(NSString *)url {
     pendingURL = [url copy];
-    NSLog(@"[CustomDeeplinks] Static pendingURL updated to: %@", pendingURL);
+    NSLog(@"[CustomDeeplinks] Static pendingURL updated: %@", pendingURL);
 }
 
 - (void)pluginInitialize {
-    NSLog(@"[CustomDeeplinks] Plugin initialized. Pending URL: %@", pendingURL);
+    NSLog(@"[CustomDeeplinks] Plugin initialized. Current pending: %@", pendingURL);
 }
 
-// Helper method to process the URL and notify JS if possible
 - (void)handleUrl:(NSString *)urlString {
     if (urlString == nil || [urlString isEqualToString:@""]) return;
 
     [CustomDeeplinksPlugin setPendingURL:urlString];
 
-    // If the WebView is ready, we dispatch the event for Warm Starts
     if (self.webViewEngine && self.webViewEngine.engineWebView) {
         NSString *js = [NSString stringWithFormat:
             @"window.dispatchEvent(new CustomEvent('deeplinks', { detail: { url: '%@' } }));", 
             urlString];
             
         [self.commandDelegate evalJs:js];
-        NSLog(@"[CustomDeeplinks] Event dispatched to JS: %@", urlString);
     }
 }
 
-// Called by AppDelegate for Universal Links
 - (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
     if (userActivity.webpageURL == nil) return NO;
-    
-    NSString *urlString = userActivity.webpageURL.absoluteString;
+    [self handleUrl:userActivity.webpageURL.absoluteString];
+    return YES;
+}
+
+- (void)getPendingDeeplink:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult *result;
+    if (pendingURL != nil && ![pendingURL isEqualToString:@""]) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:pendingURL];
+        pendingURL = nil; // Consumed
+    } else {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:nil];
+    }
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+@end    NSString *urlString = userActivity.webpageURL.absoluteString;
     NSLog(@"[CustomDeeplinks] Handling Universal Link: %@", urlString);
     
     [self handleUrl:urlString];
