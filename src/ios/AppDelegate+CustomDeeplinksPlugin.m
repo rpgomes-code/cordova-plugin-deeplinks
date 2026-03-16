3fdba663c6bd54b1a843ef6df771b4b36f0f0d62
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-03-16 11:33:38 +0000

    Update AppDelegate+CustomDeeplinksPlugin.m

--- src/ios/AppDelegate+CustomDeeplinksPlugin.m
+++ src/ios/AppDelegate+CustomDeeplinksPlugin.m
@@ -3,52 +3,37 @@
 
 @implementation AppDelegate (CustomDeeplinksPlugin)
 
-// Universal Link handler (HTTPS links)
 - (BOOL)application:(UIApplication *)application 
 continueUserActivity:(NSUserActivity *)userActivity 
 restorationHandler:(void (^)(NSArray *))restorationHandler {
 
-    NSLog(@"[CustomDeeplinks] continueUserActivity triggered");
-    
     if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
         return NO;
     }
 
     NSString *urlString = userActivity.webpageURL.absoluteString;
-    NSLog(@"[CustomDeeplinks] URL detected: %@", urlString);
-
     [CustomDeeplinksPlugin setPendingURL:urlString];
 
     if (self.viewController != nil) {
-        CustomDeeplinksPlugin *plugin = [self.viewController getCommandInstance:@"CustomDeeplinks"];
+        // O cast (CustomDeeplinksPlugin *) resolve erros de compilação
+        CustomDeeplinksPlugin *plugin = (CustomDeeplinksPlugin *)[self.viewController getCommandInstance:@"CustomDeeplinks"];
         if (plugin != nil) {
-            NSLog(@"[CustomDeeplinks] Plugin found, handling activity immediately.");
             [plugin handleUserActivity:userActivity];
-        } else {
-            NSLog(@"[CustomDeeplinks] Plugin not initialized yet. URL cached in static variable.");
         }
     }
-
     return YES;
 }
 
-// Deep link handler (Custom URL Schemes: myapp://)
-- (BOOL)application:(UIApplication *)app 
-            openURL:(NSURL *)url 
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-
-    NSLog(@"[CustomDeeplinks] openURL triggered: %@", url.absoluteString);
-
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    
     [CustomDeeplinksPlugin setPendingURL:url.absoluteString];
 
     if (self.viewController != nil) {
-        CustomDeeplinksPlugin *plugin = [self.viewController getCommandInstance:@"CustomDeeplinks"];
+        CustomDeeplinksPlugin *plugin = (CustomDeeplinksPlugin *)[self.viewController getCommandInstance:@"CustomDeeplinks"];
         if (plugin != nil) {
-            NSLog(@"[CustomDeeplinks] Notifying plugin of URL Scheme");
             [plugin handleUrl:url.absoluteString];
         }
     }
-
     return YES;
 }
 
