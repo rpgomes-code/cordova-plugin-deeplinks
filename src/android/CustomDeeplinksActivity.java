1e4a7c021c228d1511eef04c465f2138be5ad5fa
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-05-06 09:22:11 +0100

    Update CustomDeeplinksActivity.java

--- src/android/CustomDeeplinksActivity.java
+++ src/android/CustomDeeplinksActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.content.Context;
 
+import com.appsflyer.AppsFlyerLib;
+
 public class CustomDeeplinksActivity extends Activity {
 
     private static final String TAG = "[CustomDeeplinksActivity]";
@@ -41,6 +43,33 @@ public class CustomDeeplinksActivity extends Activity {
 
             startActivity(launchIntent);
             */
+            Intent incomingIntent = getIntent();
+    
+            AppsFlyerLib.getInstance().sendPushNotificationData(this.getActivity()); 
+            Uri data = incomingIntent.getData();
+        
+            if (data != null) {
+                Log.d(TAG, "Received deep link: " + data.toString());
+            }
+        
+            Context context = this;
+            String packageName = context.getPackageName();
+            Class<?> mainActivityClass = Class.forName(packageName + ".MainActivity");
+        
+            Intent launchIntent = new Intent(this, mainActivityClass);
+            launchIntent.setAction(Intent.ACTION_MAIN);
+            launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            
+            if (data != null) {
+                launchIntent.putExtra("deeplink_url", data.toString());
+            }
+            
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            if (incomingIntent.getExtras() != null) {
+                launchIntent.putExtras(incomingIntent.getExtras());
+            }
+        
+            startActivity(launchIntent);
         } catch (Exception e) {
             Log.e(TAG, "Error handling deep link intent", e);
         } finally {
